eced6c2a9d252d1bc595e07d6ed3a19e9f9e6d95
Matthijs Alles <admin@bixie.nl>
2014-02-07 00:00:16 +0100

    uikit

--- application.php
+++ application.php
@@ -14,22 +14,10 @@ class BusinessApplication extends Application {
 	 */
 	public function dispatch() {
 
-		/**
-		 * Let's load extra controllers
-		 
-		if ($path = $this->app->path->path('applications:demo/controllers')) {
-			$this->app->path->register($path, 'controllers');
-		}
-*/
-		/**
-		 * Let's bind an event to each category loading
-		 
-		$this->app->event->dispatcher->connect('category:init', array($this, 'categoryLoaded'));
-*/
 		//add uikit
+		JFactory::getDocument()->addScript($this->getURI().'/uikit/js/uikit.min.js');
+		JFactory::getDocument()->addStylesheet($this->getURI().'/uikit/css/uikit.css');
 		
-		$this->app->document->addScript('libraries:jquery/jquery-ui.custom.min.js');
-		$this->app->document->addStylesheet('libraries:jquery/jquery-ui.custom.css');
 		parent::dispatch();
 	}
 
